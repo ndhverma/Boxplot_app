39eddd4c286e8f7d643f6c7283dd1cdddb3d1e7c
Nidhi Verma <43364419+ndhverma@users.noreply.github.com>
2023-04-06 19:13:22 -0500

    Update bp_test3.py

--- bp_test3.py
+++ bp_test3.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
